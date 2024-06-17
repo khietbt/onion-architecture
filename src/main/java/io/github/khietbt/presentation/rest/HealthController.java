16b7ead3e686f0e412ce05f6610c49d6ee095821
khietbt <khietbt@gmail.com>
2024-06-17 15:26:09 +0700

    Revises the base health endpoint

---
+++ src/main/java/io/github/khietbt/presentation/rest/HealthController.java
@@ -0,0 +1,15 @@
+package io.github.khietbt.presentation.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequestMapping("/health")
+@RestController
+public class HealthController {
+    @GetMapping
+    public Mono<String> getOne() {
+        return Mono.just("OK");
+    }
+}
